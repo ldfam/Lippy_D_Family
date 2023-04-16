84fe93b30ba849c09d3925a35738802352c060d9
ldfam <125974738+ldfam@users.noreply.github.com>
2023-04-16 23:19:13 +0900

    Add files via upload

--- js/about.js
+++ js/about.js
@@ -1,6 +1,6 @@
 const aboutJS = {
     temp: (data) => `<div class="lippyD-about" data-about="${data.query}">
-                        <h3>${data.name} / ${data.eng}</h3>
+                        <h3><span>${data.name} / ${data.eng}</span></h3>
                         <div class="lippyD-about-wrapper">
                         <div class="lippyD-about-content1">
                             <div class="lippyD-about-set">
@@ -23,9 +23,10 @@ const aboutJS = {
                         <div class="lippyD-about-content2">
                             <div id="imgs">
                                 <img src="${data.img}" alt="" width="300" height="300">
-                                <div>
+                                <div class="blue-bird">
                                     <a href="https://twitter.com/${data.tw}">
-                                        <img src="https://drive.google.com/uc?export=view&id=1gxz2CYGmtEoF0W_JNIll4R3UlB9YUtam" alt="logo" width="30" height="30"><span>${data.tw}</span>
+                                        <img src="https://drive.google.com/uc?export=view&id=1gxz2CYGmtEoF0W_JNIll4R3UlB9YUtam" alt="logo" width="30" height="30">
+                                        <span class="tw-id">${data.tw}</span>
                                     </a>
                                 </div>
                             </div>
