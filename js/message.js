698f6def643de36e4d25069aea0d5ef2beb27902
ldfam <125974738+ldfam@users.noreply.github.com>
2023-05-23 00:14:24 +0900

    Add files via upload

--- js/message.js
+++ js/message.js
@@ -7,16 +7,21 @@ const messageJS = {
         const html = messageData.reduce((text, data) => {
             const mintext = data.mes.slice(0, 80);
             return text +
-                `<div class="fadeUpTrigger li">
+                `<li class="fadeUpTrigger li">
+                <a href="#modal_mes" class="modal-open mes-open" data-id='${data.id}'>
                 <div class="name">${data.name}</div>
-                <div class="minmes">${mintext}...</div>
-                </div>`
+                <div class="minmes">${mintext}...</div></a>
+                </li>`
         }, "");
         const wrap = document.getElementById("lippyD-mes-ul");
         wrap.innerHTML = html;
     },
     addEvent: () => {
-
+        const fn = (e) => {
+            console.log(e.currentTarget.dataset.id)
+        }
+        const els = document.querySelectorAll(".mes-open");
+        els.forEach(el => el.addEventListener("click", fn));
     },
 }
 messageJS.init();
